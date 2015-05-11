204ebb03179d44249a484f3c8bddf82e163ae81f
AllMixedUpCoding <nenglish0820@gmail.com>
2015-05-11 08:55:41 -0400

    Update index.php

--- app/langs/en-US/index.php
+++ app/langs/en-US/index.php
@@ -1,1 +1,10 @@
-
+<?php
+include "../../../../../../app/langs/en-US/includes/configuration.php";
+include "../../../../../../app/langs/en-US/includes/functions.php";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title ng-bind="title +' - Vawave'"></title>
+  </head>
+</html>
